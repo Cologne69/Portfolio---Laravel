a6a7d968756e48a9b04433da0c1f0106d1d42d84
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-03-06 22:25:01 +0700

    Revert "Update #2"
    This reverts commit 505e589244edec78aa12e2e17e719a59db3c0bfc.

--- resources/views/livewire/project.blade.php
+++ resources/views/livewire/project.blade.php
@@ -1,16 +1,3 @@
 <div>
     <h1 class="text-xl">Welcome to my Porfolio</h1>
-    <div class="grid grid-cols-3 gap-4">
-        <div class="bg-primary hover:shadow-xl overflow-hidden">
-            <div class="px-6 py-4">
-                <h1 class="text-lg font-semibold text-gray-700">Title</h1>
-                <p class="text-gray-500 text-sm mt-1">Desc</p>
-            </div>
-            <div class="px-6 py-4">
-                <a href="/"
-                    class="block text-center w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-3">View
-                    Project</a>
-            </div>
-        </div>
-    </div>
 </div>
