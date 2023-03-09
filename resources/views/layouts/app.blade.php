15b08f16db82297c8158f8b23f98f3a06dea1fdc
Kyrell <kleanosiauw@student.ciputra.ac.id>
2023-03-09 14:26:47 +0700

    draft #17

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ __('Kyrell | Portfolio') }}</title>
     @vite('resources/css/app.css')
-    {{-- <link rel="stylesheet" href="{{asset('resources/css/app.css')}}"> --}}
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     @livewireStyles
 </head>
 
@@ -22,16 +21,33 @@
             </div>
             <ul class="menu menu-horizontal bg-primary rounded ml-2 ">
                 <livewire:switch-language />
-
-                <li>
-                    <button onclick="toggleTheme()">
+                <li class="dropdown dropdown-bottom dropdown-end">
+                    <label tabindex="0">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                         </svg>
-                    </button>
+                    </label>
+                    <ul tabindex="0" class="dropdown-content shadow bg-base-100 rounded-box">
+                        <li><a onclick="toggleTheme('light')">Light</a></li>
+                        <li><a onclick="toggleTheme('dark')">Dark</a></li>
+                        <li><a onclick="toggleTheme('cyberpunk')">Cyberpunk</a></li>
+                        <li><a onclick="toggleTheme('night')">Night</a></li>
+                    </ul>
                 </li>
+                {{-- <li tabindex="0" style="z-index: 1" class="right-0">
+                    <a>
+
+                    </a>
+
+                    <ul class="p-2 bg-base-100">
+                        <li><a onclick="toggleTheme('light')">Light</a></li>
+                        <li><a onclick="toggleTheme('dark')">Dark</a></li>
+                        <li><a onclick="toggleTheme('cyberpunk')">Cyberpunk</a></li>
+                        <li><a onclick="toggleTheme('night')">Night</a></li>
+                    </ul>
+                </li> --}}
             </ul>
         </div>
         <livewire:tabs>
@@ -42,20 +58,16 @@
     </main>
 
     <script>
-        function toggleTheme() {
-            //change data-theme attribute
-            if (document.body.getAttribute('data-theme') == 'cyberpunk') {
-                document.body.setAttribute('data-theme', 'synthwave');
-                localStorage.setItem('theme', 'synthwave');
-            } else {
-                document.body.setAttribute('data-theme', 'cyberpunk');
-                localStorage.setItem('theme', 'cyberpunk');
+        function toggleTheme(option) {
+            let theme = document.documentElement.getAttribute('data-theme');
+            if (option) {
+                theme = option;
             }
+            document.documentElement.setAttribute('data-theme', option);
+            localStorage.setItem('theme', option);
         }
-        if (localStorage.getItem('theme') == 'cyberpunk') {
-            document.body.setAttribute('data-theme', 'cyberpunk');
-        } else {
-            document.body.setAttribute('data-theme', 'synthwave');
+        if (localStorage.getItem('theme')) {
+            toggleTheme(localStorage.getItem('theme'));
         }
     </script>
     <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,3 +1,36 @@
 <div>
 
+    {{-- <div>
+            HI
+
+        </div>
+        <div class="divider lg:divider-horizontal"></div>
+        <div class="divider divider-horizontal">OH</div>
+        <div>
+
+            HI2
+        </div> --}}
+    <div class="columns-2 ">
+        <div class="h-fit card bg-base-300 rounded-box place-items-center">
+
+        </div>
+        <div class="h-fit card bg-base-300 rounded-box place-items-center animate__animated animate__fadeInUp">
+            <form wire:submit.prevent="submitForm">
+                <div class="grid gap-4">
+                    <div>
+                        <input label="{{ __('Name') }}" model="name" required />
+                    </div>
+                    <div>
+                        <input type="email" label="{{ __('Email') }}" model="email" required />
+                    </div>
+                    <div>
+                        <textarea label="{{ __('Message') }}" model="message" required></textarea>
+                    </div>
+                    <div>
+                        <button type="submit">{{ __('Submit') }}</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
